5f00938d21fef26e1f336039d07c27e2dd4e023f
Sonali Chada <chadhass@uci.edu>
2024-01-17 20:06:44 -0800

    Submission commit of HW1.

--- src/convert_knots.hpp
+++ src/convert_knots.hpp
@@ -1,11 +1,4 @@
 double knots_to_miles_per_minute(int knots)
 {
-double ktofeetph = 6076; 
-double convrate = 1/5280; 
-double mph = 60; 
-
-double cmph = knots * ktofeetph * convrate; 
-double cmpm = cmph / mph; 
-
-return cmpm;
-}
+ return (knots * 6076.0) / (5280.0 * 60.0);
+ }
