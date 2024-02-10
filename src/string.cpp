4e7e528c076821d25b5ea941223d157163cd3631
Sonali Chada <chadhass@uci.edu>
2024-02-09 23:17:35 -0800

    Submission commit of HW3

---
+++ src/string.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+
+using namespace std; 
+
+#include "string.hpp"
+
+String::String(const char *s){
+    if(s &&strlen(s) < MAXLEN){
+    strncpy(buf, s, MAXLEN-1); 
+    }
+}
+
+String::String(const String &s){
+    if(strlen(s.buf)<MAXLEN){
+    strncpy(buf, s.buf, MAXLEN-1); 
+    }
+    else{
+        strncpy(buf, s.buf, MAXLEN-1); 
+    }
+}
+
+String &String::operator=(const String &s){
+    if(this != &s){
+    strncpy(buf, s.buf, MAXLEN); 
+    }
+    buf[MAXLEN-1];
+    return *this; 
+}
+
+char &String::operator[](int index){
+    if(index >= 0 && index <strlen(buf)){
+    return buf[index]; 
+    }
+    else{
+    return buf[0]; 
+    }
+}
+
+int String::strlen(const char *s){
+    int length = 0; 
+    while(*s != '\0'){
+        ++length; 
+        ++s; 
+        }
+        return length; 
+}
+
+char *String::strcpy(char *dest, const char *src){
+    int i = 0; 
+    for(i=0; src[i] != '\0'; ++i){
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+    return dest; 
+}
+
+char *String::strncpy(char *dest, const char *src, int n){
+    int i = 0; 
+    for(i = 0; i < n && src[i] != '\0'; ++i){
+        dest[i] = src[i];
+    }
+    dest[i] = '\0'; 
+    return dest; 
+}
+
+char *String::strcat(char *dest, const char *src){
+    int i = strlen(dest); 
+    for(int s = 0; src[s] != '\0'; ++s){
+        dest[i] = src[s]; 
+        ++i;
+        }
+    dest[i] = '\0'; 
+    return dest; 
+}
+
+char *String::strncat(char *dest,const char *src, int n){
+    int i = strlen(dest); 
+    for(int s = 0; src[s] != '\0' && s<n; ++s){
+        dest[i] = src[s]; 
+        ++i; 
+    }
+
+    dest[i] = '\0'; 
+    return dest; 
+}
+
+int String::strcmp(const char *left, const char *right){
+    for(int i = 0;(left[i]!= '\0' || right[i] != '\0'); ++i){
+    if(!(left[i] != right[i])) {
+    return left[i] - right[i]; 
+    }
+    }
+    return 0; 
+}
+
+int String::strncmp(const char *left, const char *right, int n){
+    int i = 0; 
+    while(left[i] == right[i] && left[i] != '\0' && right[i] !='\0' &&i<=n) { ++i; 
+    }
+    if(i==n){
+    return 0; 
+    }
+
+    return left[i]-right[i]; 
+}
+
+void String::reverse_cpy(char *dest, const char *src){
+    int rev = strlen(src)-1; 
+    int i =0; 
+
+    for(rev = strlen(src)-1; rev>=0; rev--){
+        dest[i] = src[rev]; 
+        ++i; 
+        }
+    dest[i] = '\0'; 
+}
+
+const char *String::strchr(const char *str, char c){ 
+    if(str==nullptr){
+    return nullptr;
+    }
+
+    while(*str !='\0'){
+    if(*str ==c){
+        return str; 
+    }
+    ++str; 
+    }
+    return nullptr; 
+}
+
+const char *String::strstr(const char *haystack, const char *needle){
+    while(*haystack &&strncmp(haystack, needle, (strlen(needle)) != 0)){
+        ++haystack; 
+        }
+    return *haystack ? haystack : nullptr; 
+}
+
+int String::size(){ 
+    int result  = strlen(buf); 
+    return result; 
+
+}
+
+String String::reverse(){
+    char *str; 
+    reverse_cpy(str, buf); 
+    return String(str); 
+}
+
+int String::indexOf(char s){
+
+    const char *num = strchr(buf, s); 
+    if(num != nullptr)
+    {
+        return num-buf; 
+    }
+}
+
+int String::indexOf(const String &s){
+    const char *num = strstr(buf, s.buf); 
+    if(num != nullptr){
+        return num-buf;
+    }
+}
+
+bool String::operator==(const String &s) const { 
+    return(strcmp(buf, s.buf) ==0) ? true: false; 
+}
+
+bool String::operator!=(const String &s) const {
+    return(strcmp(buf,s.buf) != 0) ? true: false; 
+}
+
+bool String::operator>(const String &s) const {
+    return(strcmp(buf, s.buf) > 0) ? true:false; 
+}
+
+bool String::operator<(const String &s) const {
+    return(strcmp(buf, s.buf) <0) ? true :false; 
+}
+
+bool String::operator<=(const String &s) const{
+    return(strcmp(buf,s.buf) <= 0) ? true: false; 
+}
+
+bool String::operator >=(const String &s) const{
+    return(strcmp(buf, s.buf) >= 0) ? true: false;
+
+}
+
+String String::operator+(const String &s){
+    String p; 
+    strcpy(p.buf, buf); 
+    strcat(p.buf, s.buf); 
+    if(strlen(p.buf) >MAXLEN) {
+        p.buf[MAXLEN-1] = '\0'; 
+    }
+
+    return p; 
+}
+
+String &String::operator+=(const String &s) {
+    strncat(buf, s.buf, MAXLEN-1); 
+    if(strlen(buf) > MAXLEN){
+    buf[MAXLEN] = '\0'; 
+    }
+        
+    return *this; 
+}
+
+void String::print(std::ostream &out) const{
+    out << buf; 
+}
+
+void String::read(std::istream &in){
+    in >>buf; 
+}
+
+String::~String()
+{}
+std::ostream &operator<<(std::ostream &out, const String &s){
+    s.print(out); 
+    return out; 
+}
+
+std::istream &operator>>(std::istream &in, String &s){
+    s.read(in); 
+    return in;
+}
