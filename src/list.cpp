b1c13b86fa04535778df989165c7c6b76f8884f8
Sonali Chada <chadhass@uci.edu>
2024-02-15 20:19:23 -0800

    Submission commit of HW5

---
+++ src/list.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include "list.hpp"
+
+namespace list{
+
+Node* from_string(const char* s){
+    if(!s || s[0] == '\0') { 
+    return nullptr; 
+    }
+
+    Node* head = new Node{.data = *s, .next = nullptr}; 
+    Node* p = head; 
+    ++s; 
+
+while(*s != '\0'){ 
+    p->next = new Node{.data = *s, .next = nullptr}; 
+    p = p->next; 
+    ++s; 
+ }
+    return head; 
+}
+
+void free(Node* head){ //delete linked list nodes
+    while(head!=nullptr){
+        Node* nextDel = head->next; 
+        delete head; 
+        head = nextDel; 
+    }
+}
+
+void print(std::ostream& out, Node*head){
+    for(Node* h = head; h!= nullptr; h=h->next){
+    out << h->data; 
+    }
+}
+
+Node* copy(Node* head){
+    if(head != nullptr){
+    Node * cHead = new Node{.data = head ->data, .next = nullptr}; 
+    Node *p = cHead; 
+    for(Node* loopP = head->next; loopP != nullptr; loopP = loopP->next){
+         cHead-> next = new Node{.data = p->data, .next = nullptr}; 
+        cHead = cHead-> next; 
+         }
+    return p; 
+    }
+    else { return nullptr; }
+}
+
+int compare(Node* lhs, Node* rhs){
+     Node* l = lhs; 
+     Node* r = rhs; 
+     while(l != nullptr && r != nullptr){
+        if(l->data != r-> data){
+        return l->data - r->data;
+        }
+    l = l->next; 
+    r = r->next; 
+
+}
+    if(l == nullptr && r == nullptr){
+    return 0; 
+    }
+    else if(l == nullptr){
+        return -1; 
+    }else{
+    return 1; 
+    }
+}
+
+int compare(Node* lhs, Node* rhs, int n){
+    Node* l = lhs; 
+    Node* r = rhs; 
+
+    for(int i = 0; i <n; i++){
+    if(!l && !r){
+    return 0; 
+    }
+    else if(!l){
+    return -1; 
+    }
+    else if(!r){
+    return 1;
+    }
+    else if(l->data !=  r->data){ 
+    return l->data - r->data; 
+    }
+    l = l-> next; 
+    r = r -> next; 
+    }
+    return 0; 
+}
+
+int length(Node* head){ 
+    if(head == nullptr) { return 0; }
+    int len = 0;
+    for(Node* loopP = head; loopP != nullptr; loopP = loopP ->next){
+        ++len; 
+    }
+    return len; 
+}
+
+Node* reverse(Node*head){
+    Node* rev = copy(head); 
+    Node* prev = nullptr; 
+    Node* rprev = rev; 
+    Node* next = nullptr; 
+
+    while(rprev != nullptr && rprev->data != '\0'){
+        next = rprev->next; 
+        rprev->next = prev; 
+        prev = rprev; 
+        rprev = next; 
+    }
+    return prev; 
+}
+
+Node* appent(Node* lhs, Node* rhs){
+    Node* l = copy(lhs); 
+    Node* r = copy(rhs); 
+
+    if(lhs == nullptr) { return r;}
+    last(l)->next = r; 
+
+    return l; 
+}
+
+int index(Node* head, Node* node){
+    Node* h = head; 
+    int index = 0; 
+    while(h!=nullptr){
+    if(compare(h, node) ==0){
+        return index; 
+    }
+    h = h->next; 
+    ++index; 
+    }
+    return -1; 
+}
+
+Node* find_char(Node*head, char c){
+    Node *h = head; 
+    while(h != nullptr){
+        if(h->data != c){
+            h  = h->next; 
+        }
+        else{
+        return h; 
+        }
+    return h; 
+}
+
+Node* find_list(Node* haystack, Node* needle){
+    if(haystack ==nullptr){ return nullptr; }
+    Node* h = haystack; 
+    Node* n = needle; 
+    const int needleLen - kength(needle); 
+    Node* find = nth(haystack, length(needle)-1); 
+    bool Nfound = false; 
+    while(find != nullptr || Nfound == true)
+    {
+        if(compare(n,h, needleLen) == 0)
+        {
+            return h; 
+        } else{
+        h = h->next; 
+        find = find->next; 
+        }
+     }
+    return nullptr; 
+}
+
+Node* nth(Node* head, int n){
+    Node* h = head; 
+    
+    for(int i = 0; i<n && h != nullptr; ++i){
+        h = h->next; 
+    }
+    return h; 
+}
+
+Node* last(Node* head){
+    if(head == nullptr) { return nullptr}
+    Node* last = head; 
+    while( last -> next != nullptr){
+        last = last->next; 
+    }
+    return last; 
+}
+}
+
---
+++ src/string.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include "string.hpp"
+
+
+String::String(list::Node*head){ //default empty string 
+    this->head = head; 
+}
+
+String::String(const char *s){
+    head = list::from_string(s); 
+}
+
+//construct the string as a copy s 
+String::String(const String &s){
+    head = list::copy(s.head); 
+}
+
+String::String(String &&s){ 
+    head = s.head; 
+    s.head = nullptr; 
+    list::free(s.head); 
+}
+
+void String::swap(String &s){
+    list::Node* sw = s.head; 
+    s.head = head; 
+    head = sw; 
+}
+
+//assignment operator for strings 
+String &String::operator= (const String &s){
+if( list::compare(head, s.head) != 0){ 
+    list::free(head); 
+    head = list::copy(s.head); 
+}
+return *this; 
+}
+
+//assign by moving 
+String &String::operator=(String &&s){
+    if(head != s.head){ 
+    list::free(head); 
+    head = s.head; 
+    s.head = nullptr; 
+}
+return *this; 
+}
+
+bool String::in_bounds(int index) const { 
+    return(indxex< list::length(head)); 
+}
+
+char String::operator[](int index) const{
+    if(in_bounds(index)){
+        return list::nth(head, index)-> data; 
+    }
+    else{
+    return '\0'; 
+    }
+}
+
+int String::size() const{
+    return list::length(this->head); 
+}
+
+String String::reverse() const{
+    String rev; 
+    rev.head = list::reverse(this->head); 
+    return rev; 
+}
+
+int String::indexOf(char c) const {
+    int index = list::find_char(this->head, c);
+    return list::index(this->head, index); 
+}
+
+int String::indexOf(const String &s) const{ 
+    int index = list::find_list(this->head, s.head); 
+    return list::index(this->head, index); 
+}
+
+//compare string(NOT assign) 
+bool String::operator==(const String &s) const{ 
+    return list::compare(this->, s.head) ==0; 
+}
+
+
+std::strong_orderderingString::operator<=>(const String &s) const{
+    return list::compare(head, s.head) <=> 0; 
+} 
+
+Sting Stirng::operator+(const String &s) const{ 
+    String add; 
+    add.head = list::append(this->head, s.head); 
+    list::free(head); 
+    return add; 
+}
+
+String &String::operator+=(const String &s){
+    head = list::append(this->head, s.head); 
+    return *this; 
+} 
+
+void String::print(std::ostream &out) const{
+    list::print(out, head); 
+}
+
+void String::read(std::istream &in){ 
+    char Input[1000000]; 
+    in >> Input; 
+    list::free(head); 
+    head = list::from_string(Input); 
+}
+
+String::~String(){
+    list::free(this->head); 
+} 
+
+std::ostream &operator<<(std::ostream &out, const String &s){
+    s.print(out); 
+    return out; 
+}
+
+std::istream &operator>>(std::istream &in, String &s){
+    s.read(in); 
+    return in; 
+}
+
