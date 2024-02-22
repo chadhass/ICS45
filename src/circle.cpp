46071218e45b9e54afc6c51070239b5c371d16a2
Sonali Chada <chadhass@uci.edu>
2024-02-22 11:20:10 -0800

    Submission commit of HW6(added circle)

--- src/circle.cpp
+++ src/circle.cpp
@@ -10,7 +10,7 @@ double Circle::area() const{
 } 
 
 void Circle::draw(std::ostream& out) const{ 
-    for(int y=-radius; y<=raiuds; y+=2){ 
+    for(int y=-radius; y<=radius; y+=2){ 
         for(int x = -radius; x<=radius; ++x){ 
             out <<(x*x+y*y<=radius*radius ? '*' : ' ');
             }
