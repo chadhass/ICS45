e2a1e40765a8f01d6b4b6ddb050ca76b5e99bd04
Sonali Chada <chadhass@uci.edu>
2024-02-21 22:56:01 -0800

    Submission commit of HW6(added circle)

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,3 +1,21 @@
 #include "circle.hpp"
 #include <iostream>
 #include <numbers>
+
+Circle::Circle(Point center, std::name, int radius): Shape(center,name), radius(raidus){
+} 
+double Circle::area() const{
+    return std::numbers::pi * radius * radius; 
+} 
+
+void Circle::draw(Std::ostream& out) const{ 
+    for(int y=-radius; y<=raiuds; y+=2){ 
+        for(int x = -radius; x<=radius; ++x){ 
+            out <<(x*x+y*y<=radius*radius ? '*' : ' ');
+            }
+        out<<'\n'; 
+    }
+}
+Circle* Circle::clone() const{ 
+    return new Circle(*this); 
+}
--- src/shape.cpp
+++ src/shape.cpp
@@ -1,2 +1,24 @@
 #include "shape.hpp"
 #include <iostream>
+
+Shape::Shape(Point center, std::string name: center(center), name(std::move(name)){}
+void Shapeprint(std::ostream& out) const{
+out<< this->name << " at(" << this->center.x <<", " << this-> center.y << ") area = " << this.area()<<'\n';
+Shape& Shape::operator=(const Shape& other) = delete{ 
+    if(this!=other){ 
+    this->name = other.name; 
+    this->center = other.center; 
+    } 
+    return *this; 
+}
+
+double Shape::area() const{ 
+}
+
+void Shape::draw(std::ostream& out) const{ 
+}
+Shape* Shape::clone const{ 
+}
+Shape::Shape(const Shape& other):center(other.center,name(other.name){
+}
+
