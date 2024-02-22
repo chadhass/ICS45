77d928a0b281f028c1489406cc52dda2235c9a1f
Sonali Chada <chadhass@uci.edu>
2024-02-22 10:17:46 -0800

    Submission commit of HW6(added circle)

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,18 +1,23 @@
 #include "shape.hpp"
 #include <iostream>
 
-Shape::Shape(Point center, std::string name): center(center), name(std::move(name)){}
+Shape::Shape(Point center, std::string name): center(center),name(std::move(name))
+{
+}
 void Shape::print(std::ostream& out) const{
-out<< this->name << " at(" << this->center.x <<", " << this-> center.y << ") area = " << this->area()<<'\n';
+out<< this->name << " at(" << this->center.x <<", " << this-> center.y << ") area = " <<
+this->area()<<'\n';
 }
 
-double Shape::area() const{ 
+double Shape::area() const
+{
+return 0.0; 
 }
 
-void Shape::draw(std::ostream& out) const{ 
+void Shape::draw(std::ostream& out) const
+{ 
 }
 Shape* Shape::clone const{ 
-}
-Shape::Shape(const Shape& other):center(other.center,name(other.name){
+return nullptr; 
 }
 
