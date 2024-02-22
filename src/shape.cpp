bc4daf6ff52dc7663d94811d69f77cbdf38f76b5
Sonali Chada <chadhass@uci.edu>
2024-02-22 10:11:05 -0800

    Submission commit of HW6(added circle)

--- src/shape.cpp
+++ src/shape.cpp
@@ -5,13 +5,6 @@ Shape::Shape(Point center, std::string name): center(center), name(std::move(nam
 void Shape::print(std::ostream& out) const{
 out<< this->name << " at(" << this->center.x <<", " << this-> center.y << ") area = " << this->area()<<'\n';
 }
-Shape& Shape::operator=(const Shape& other) = delete{ 
-    if(this!=other){ 
-    this->name = other.name; 
-    this->center = other.center; 
-    } 
-    return *this; 
-}
 
 double Shape::area() const{ 
 }
