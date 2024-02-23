e5db81d0061e4f0da9b1e36c4d21bacc4f6afad8
Sonali Chada <chadhass@uci.edu>
2024-02-22 20:26:56 -0800

    Submission commit of HW6(added circle)

--- src/square.hpp
+++ src/square.hpp
@@ -3,7 +3,7 @@
 // class Square 
 class Square: public Rectangle{ 
     public: 
-    using Rectangle::Recangle; 
+    using Rectangle::Rectangle; 
     Square(Point center, std::string name, int side); 
     private:
     int side; 
