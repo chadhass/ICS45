dd34ee6314d9fa0b55f935f1aeae4ffd59aefe50
Sonali Chada <chadhass@uci.edu>
2024-02-22 20:39:08 -0800

    Submission commit of HW6(added circle)

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -13,7 +13,7 @@ double Triangle::area() const{
 void Triangle::draw(std::ostream&out) const{
     int img = std::max(height, base); 
     for(int i =0; i<height; i+=2){ 
-        for(int j = 0; j(img-i)/2; ++j){ 
+        for(int j = 0; j<(img-i)/2; ++j){ 
             out << ' '; 
         }
         for(int k =0; k <= i; ++k){
