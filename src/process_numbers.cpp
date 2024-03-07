2f304270d9d4922651b539f56523ea13ff2e5c0c
Sonali Chada <chadhass@uci.edu>
2024-03-06 17:04:57 -0800

    p1

--- src/process_numbers.cpp
+++ src/process_numbers.cpp
@@ -10,16 +10,18 @@ void split_odd_even(std::istream& numbers, std::ostream& odds, std::ostream& eve
 std::vector<int> nums{
     std::istream_iterator<int>(numbers), std::istream_iterator<int>()}; 
 } 
+
+std::sort(nums.begin(), nums.end()); 
+
 std::copy_if(nums.begin(), nums.end(),std::ostream_interator<int>(odds," "),[](int num)
 { 
     n = num%2 != 0;
     return n; 
 }); 
 
-std::copy_if(nums.begin(),nums.end(),std::ostream_iterator<int>(events, " ")[](int num){ 
+std::copy_if(nums.begin(),nums.end(),std::ostream_iterator<int>(evens, " ")[](int num){ 
     n = num%2 ==0; 
     return n; 
 }); 
 
-std::sort(nums.begin(), nums.end()); 
 } 
