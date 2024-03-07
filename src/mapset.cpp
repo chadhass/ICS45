b9199221465c03a42db29e1d8dc99533a1799f2a
Sonali Chada <chadhass@uci.edu>
2024-03-06 22:26:52 -0800

    p2

--- src/mapset.cpp
+++ src/mapset.cpp
@@ -25,7 +25,7 @@ std::set<std::string> load_stopwords(std::istream& stopwords){
 
 }
 
-std::map<std::string, int> countwords(std::istream& document, const std::set<std::string>& stopwords){
+std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords){
     std::map<std::string, int> wordmap; 
 
     std::vector<std::string> words; 
