65ba48ecbd5e70c80fb8a68ed46eebe96de2afe6
Sonali Chada <chadhass@uci.edu>
2024-03-13 23:20:53 -0700

    map

--- src/map_array.hpp
+++ src/map_array.hpp
@@ -82,7 +82,7 @@ public:
     using iterator = ArrayIterator; 
     //Normally there would also be a const_iterator 
 
-    (ArrayIterator begin(){ 
+    ArrayIterator begin(){ 
         return ArrayIterator(data.data());
     }
     ArrayIterator end(){ 
